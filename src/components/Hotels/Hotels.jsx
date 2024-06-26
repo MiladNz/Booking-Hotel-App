d20a493e38b5a037be3af80f1bf6111d3a6824bc
miladn <milad.n91@gmail.com>
2024-06-26 17:01:45 +0330

    filter based on search params

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import Loader from "../Loader/Loader";
 
@@ -12,7 +12,28 @@ function Hotels() {
   );
 
   if (isLoading) <Loader />;
-  return <div>{data.length}</div>;
+  return (
+    <div className="searchList">
+      <h2>Search Results({data.length})</h2>
+      {data.map((item) => {
+        return (
+          <Link
+            key={item.id}
+            to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}>
+            <div className="searchItem">
+              <img src={item.picture_url.url} alt={item.name} />
+              <div className="searchItemDesc">
+                <p className="location">{item.smart_location}</p>
+                <p className="name">{item.name}</p>
+                <p className="price">€&nbsp;{item.price}&nbsp;</p>
+                <span>night </span>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Hotels;
